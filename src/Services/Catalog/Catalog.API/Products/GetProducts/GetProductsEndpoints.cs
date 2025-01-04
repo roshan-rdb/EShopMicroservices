be676a2639b91b40a92b8beb46f00cebe11fff27
roshan-rdb <roshan.bobade@outlook.com>
2025-01-04 15:03:31 +0530

    Get products api

--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoints.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoints.cs
@@ -1,6 +1,22 @@
-﻿namespace Catalog.API.Products.GetProducts
+﻿
+namespace Catalog.API.Products.GetProducts
 {
-    public class GetProductsEndpoints
+    public record GetProductResponse(IEnumerable<Product> Products);
+    public class GetProductsEndpoints : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products", async (ISender sender) =>
+            {
+                var result = await sender.Send(new GetProductsQuery());
+                var response = result.Adapt<GetProductResponse>();
+                return Results.Ok(response);
+            })
+            .WithName("GetProducts")
+            .Produces<GetProductResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Products")
+            .WithDescription("Get Products");
+        }
     }
 }
