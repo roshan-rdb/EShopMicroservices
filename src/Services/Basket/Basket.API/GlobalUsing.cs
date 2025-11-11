2dbe93dd52278afed58211032102b6015a88ce67
roshan-rdb <roshan.bobade@outlook.com>
2025-11-11 19:55:16 +0530

    Initial commits for basket service

---
+++ src/Services/Basket/Basket.API/GlobalUsing.cs
@@ -0,0 +1,11 @@
+﻿global using Basket.API.Models;
+global using BuildingBlock.CQRS;
+global using Carter;
+global using MediatR;
+global using Mapster;
+global using FluentValidation;
+global using BuildingBlock.Behaviors;
+global using BuildingBlock.Exceptions;
+global using Basket.API.Exceptions;
+global using Marten;
+global using Basket.API.Data;
---
+++ src/Services/Basket/Basket.API/Program.cs
@@ -0,0 +1,27 @@
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add Services to the container.
+var assembly = typeof(Program).Assembly;
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssemblies(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+//Data Services
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+    opts.Schema.For<ShoppingCart>().Identity(x => x.UserName);
+}).UseLightweightSessions();
+
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.MapCarter();
+
+app.Run();
